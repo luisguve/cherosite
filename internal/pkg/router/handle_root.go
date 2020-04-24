46f0239560c21e5bcbe2ca74e076099fdfffc699
Luis G. Villegas <luisguveal@mail.com>
2020-04-24 17:58:50 -0430

    added read notifications handler

--- internal/pkg/router/handle_root.go
+++ internal/pkg/router/handle_root.go
@@ -311,3 +311,33 @@ func (r *Router) handleExploreRecycle(w http.ResponseWriter, req *http.Request) 
 		http.Error(w, "INTERNAL_FAILURE", http.StatusInternalServerError)
 	}
 }
+
+// Read Notifications "/readnotifs" handler. It returns OK on success or an error
+// in case of the following:
+// - user is unregistered -> USER_UNREGISTERED
+// - network failures -----> INTERNAL_FAILURE
+func (r *Router) handleReadNotifs(userId string, w http.ResponseWriter, 
+	req *http.Request) {
+	request := &pb.ReadNotifsRequest {
+		UserId: userId,
+	}
+	_, err := r.crudClient.MarkAllAsRead(context.Background(), request)
+	if err != nil {
+		if resErr, ok := status.FromError(err); ok {
+			switch resErr.Code() {
+			case codes.Unauthenticated:
+				log.Println(resErr.Message())
+				http.Error(w, "USER_UNREGISTERED", http.StatusUnauthorized)
+				return
+			default:
+				http.Error(w, "INTERNAL_FAILURE", http.StatusInternalServerError)
+				return
+			}
+		}
+		log.Printf("Could not send request: %v\n", err)
+		http.Error(w, "INTERNAL_FAILURE", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
