6f6fde05023170dff6aa1ee15765d7a24e4ed81a
Luis G. Villegas <luisguveal@mail.com>
2020-04-29 11:56:02 -0430

    extended DiscardIds struct to include activity of current user, threads saved and activity of users following

--- internal/pkg/pagination/pagination.go
+++ internal/pkg/pagination/pagination.go
@@ -9,9 +9,13 @@ func RegisterSessionTypes() {
 }
 
 type DiscardIds struct {
-	// FeedThreads holds the set of thread ids the user has already seen in its 
-	// dashboard feed.
-	FeedThreads    []string
+	// UserActivity holds threads, comments and subcomments created by the user that 
+	// she has already seen in its feed
+	UserActivity   Activity
+	// FeedActivity maps user ids of authors of threads, comments and subcomments 
+	// to ids of these kinds of content that compose the feed of the current user
+	// that she has already seen
+	FeedActivity   map[string]Activity
 	// SectionThreads maps section names to the threads the user has already seen
 	// in the section
 	SectionThreads map[string][]string
@@ -20,6 +24,30 @@ type DiscardIds struct {
 	ThreadComments map[string][]string
 	// GeneralThreads maps section names to threads ids.
 	GeneralThreads map[string][]string
+}
+
+type Activity struct {
+	Subcomments    []Subcomment
+	Comments       []Comment
+	ThreadsCreated []Thread
+}
+
+// A thread is in a section and has an id
+type Thread struct {
+	SectionName string
+	Id          string
+}
+
+// A comment is in a thread and has an id
+type Comment struct {
+	Thread
+	Id string
+}
+
+// A subcomment is in a comment and has an id
+type Subcomment struct {
+	Comment
+	Id string
 }
 
 // thread ids are canonical
