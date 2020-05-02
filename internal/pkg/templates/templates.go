a6ec14b65db989aa414a26e80f606be8b1adfff8
Luis G. Villegas <luisguveal@mail.com>
2020-05-01 19:57:14 -0430

    defined new data types for easy field access in templates

--- internal/pkg/templates/templates.go
+++ internal/pkg/templates/templates.go
@@ -1,10 +1,14 @@
 package templates
 
 import(
+	"fmt"
+	"strings"
 	"html/template"
 	
 	pb "github.com/luisguve/cheropatilla/internal/cheropatillapb"
 )
+
+var tpl *template.Template
 
 type UserInfo struct {
 	Alias           string
@@ -23,13 +27,155 @@ type DashboardView struct {
 	Feed           FeedContent `json="feed_content"`
 }
 
-type ProfileView struct {
-	Username       string
-	BasicData      UserInfo
-	ThreadsCreated []*pb.FullContentData `json="threads_created"`
-	Following      uint32 `json="following"`
-	Followers      uint32 `json="followers"`
+type Content interface {
+	// Render returns the HTML representation of this content, according to its kind
+	// and status.
+	Render() template.HTML
+	// Kind returns the content type, which may be thread, comment or subcomment
+	Kind() string
+	// Status returns the status of the content, which may be NEW, RELEVANT or TOP
+	Status() string
 }
+
+// BasicContent is the set of fields that are shared by all the kinds of content:
+// threads, comments and subcomments
+type BasicContent struct {
+	ContentType   string // thread, comment or subcomment
+	ContentStatus string // NEW, RELEVANT or TOP
+	Thumbnail     string // Thumbnail URL
+	Permalink     string // Content URL
+	Content       string
+	Summary       string
+	Topvotes      uint32
+	Topvoted      bool // Has the current user topvote'd this content?
+	SectionName   string
+	Author        string // User alias
+	PublishDate   string
+	ThreadLink    string // Thread URL. It includes SectionLink
+	SectionLink   string // Section URL
+}
+
+func (b *BasicContent) Kind() string {
+	return b.ContentType
+}
+
+func (b *BasicContent) Status() string {
+	return b.ContentStatus
+}
+
+type ThreadView struct {
+	*BasicContent
+	Title   string
+	Replies uint32
+}
+
+func (t *ThreadView) Render() template.HTML {
+	var name string
+	switch t.BasicContent.Status() {
+	case "NEW":
+		name = "newthread.html"
+	case "RELEVANT":
+		name = "relthread.html"
+	case "TOP":
+		name = "topthread.html"
+	}
+	result := new(strings.Builder)
+	if err := tpl.ExecuteTemplate(result, name, t); err != nil {
+		return fmt.Sprintf("Could not execute template %s: %v\n", name, err)
+	}
+	return template.HTML(result.String())
+}
+
+type CommentView struct {
+	*BasicContent
+	Id      string
+	Replies uint32
+}
+
+func (c *CommentView) Render() template.HTML {
+	var name string
+	switch c.BasicContent.Status() {
+	case "NEW":
+		name = "newcomment.html"
+	case "RELEVANT":
+		name = "relcomment.html"
+	case "TOP":
+		name = "topcomment.html"
+	}
+	result := new(strings.Builder)
+	if err := tpl.ExecuteTemplate(result, name, c); err != nil {
+		return fmt.Sprintf("Could not execute template %s: %v\n", name, err)
+	}
+	return template.HTML(result.String())
+}
+
+type SubcommentView struct {
+	*BasicContent
+	CommentId string
+	Id        string
+}
+
+func (sc *SubcommentView) Render() template.HTML {
+	var name string
+	switch sc.BasicContent.Status() {
+	case "NEW":
+		name = "newsubcomment.html"
+	case "RELEVANT":
+		name = "relsubcomment.html"
+	case "TOP":
+		name = "topsubcomment.html"
+	}
+	result := new(strings.Builder)
+	if err := tpl.ExecuteTemplate(result, name, sc); err != nil {
+		return fmt.Sprintf("Could not execute template %s: %v\n", name, err)
+	}
+	return template.HTML(result.String())
+}
+
+type Notif struct {
+	Permalink string
+	Title     string
+	Message   string
+	Date      string
+}
+
+type CurrentUser struct {
+	Alias         string
+	Notifications []*Notif
+	// IsFollower indicates whether the current user is following another user,
+	// in a context in which it is viewing another user's profile or content
+	IsFollower    bool
+}
+
+type ProfileData struct {
+	Patillavatar string // URL to user profile pic
+	Alias        string
+	Username     string
+	Followers    uint32
+	Following    uint32
+	Description  string
+	Activity     []*Content
+}
+
+type ProfileView struct {
+	CurrentUser
+	ProfileData
+}
+
+type CurrentUserData struct {
+	Followers    uint32
+	Following    uint32
+	Activity     []*Content
+	SavedContent []*Content
+}
+
+type DashboardView struct {
+	CurrentUser
+	CurrentUserData
+	Feed []*Content
+}
+
+/*old below*/
 
 type ThreadView struct {
 	Username string `json="username"`
@@ -62,6 +208,7 @@ type FeedSubcomments struct {
 	ErrorMsg    string `json="error_message"`
 }
 
-func New() *template.Template {
-	return template.Must(template.ParseGlob("*.html"))
+func Setup() *template.Template {
+	tpl = template.Must(template.ParseGlob("/web/internal/templates/*.html"))
+	return template.Must(template.ParseGlob("/web/templates/*.html"))
 }
