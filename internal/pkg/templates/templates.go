b86d3e273ac405737d92ecb80fcd9b6845c8683c
Luis G. Villegas <luisguveal@mail.com>
2020-05-03 16:59:18 -0430

    removed unused type definitions

--- internal/pkg/templates/templates.go
+++ internal/pkg/templates/templates.go
@@ -4,28 +4,9 @@ import(
 	"fmt"
 	"strings"
 	"html/template"
-	
-	pb "github.com/luisguve/cheropatilla/internal/cheropatillapb"
 )
 
 var tpl *template.Template
-
-type UserInfo struct {
-	Alias           string
-	Username        string
-	PicUrl          string
-	About           string
-	LastTimeCreated int64
-}
-
-type DashboardView struct {
-	FullUserData   *pb.FullUserData `json="user_data"`
-	ThreadsCreated []*pb.FullContentData `json="threads_created"`
-	ThreadsSaved   []*pb.ThreadData `json="threads_saved"`
-	Following      uint32 `json="following"`
-	Followers      uint32 `json="followers"`
-	Feed           FeedContent `json="feed_content"`
-}
 
 type Content interface {
 	// Render returns the HTML representation of this content, according to its kind
@@ -213,39 +194,6 @@ type ThreadView struct {
 type MyProfileView struct {
 	HeaderData
 	ProfileData
-}
-
-/*old below*/
-
-type ThreadView struct {
-	Username string `json="username"`
-	Content  *pb.FullContentData `json="content"`
-	Feed     FeedContent `json="feed_content"`
-}
-
-type SectionView struct {
-	Username string `json="username"`
-	Feed     FeedContent `json="feed_content"`
-}
-
-type FeedContent struct {
-	Feed
-	ContentIds []string `json="-"`
-}
-
-type FeedGeneral struct {
-	Feed
-	ContentIds map[string][]string `json="-"`
-}
-
-type Feed struct {
-	ContentPattern []*pb.ContentRuleResponse `json="content_pattern"`
-	ErrorMsg       string `json="error_message"`
-}
-
-type FeedSubcomments struct {
-	Subcomments []*pb.Subcomment `json="subcomments"`
-	ErrorMsg    string `json="error_message"`
 }
 
 func Setup() *template.Template {
