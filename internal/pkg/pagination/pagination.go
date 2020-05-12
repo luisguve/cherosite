0ca450ebcaf60c32f2b74cb146e3487bc0ec1c09
Luis G. Villegas <luisguveal@mail.com>
2020-05-12 08:59:26 -0430

    created new method to convert DiscardId into map[string]*pb.FullUserData_Activity to discard ids

--- internal/pkg/pagination/pagination.go
+++ internal/pkg/pagination/pagination.go
@@ -11,7 +11,7 @@ func RegisterSessionTypes() {
 type DiscardIds struct {
 	// UserActivity holds threads, comments and subcomments created by the user that 
 	// she has already seen in its feed
-	UserActivity   Activity
+	UserActivity   map[string]Activity
 	// FeedActivity maps user ids of authors of threads, comments and subcomments 
 	// to ids of these kinds of content that compose the feed of the current user
 	// that she has already seen
@@ -27,6 +27,54 @@ type DiscardIds struct {
 	ThreadComments map[string][]string
 	// GeneralThreads maps section names to threads ids.
 	GeneralThreads map[string][]string
+}
+
+func (d *DiscardIds) FormatUserActivity(userId string) 
+map[string]*pb.FullUserData_Activity {
+	var fudActivity *pb.FullUserData_Activity
+
+	// Set threads
+	for t := range d.UserActivity[userId].ThreadsCreated {
+		pbThread := &pb.Context_Thread{
+			Id:         t.Id,
+			SectionCtx: &pb.Context_section{
+				Name: t.SectionName,
+			},
+		}
+		fudActivity.ThreadsCreated = append(fudActivity.ThreadsCreated, pbThread)
+	}
+	// Set comments
+	for c := range d.UserActivity[userId].Comments {
+		pbComment := &pb.Context_Comment{
+			Id:        c.Id,
+			ThreadCtx: &pb.Context_Thread{
+				Id:         c.Thread.Id,
+				SectionCtx: &pb.Context_section{
+					Name: c.Thread.SectionName,
+				},
+			},
+		}
+		fudActivity.Comments = append(fudActivity.Comments, pbComment)
+	}
+	// Set subcomments
+	for sc := range d.UserActivity[userId].Subcomments {
+		pbSubcomment := &pb.Context_Subcomment{
+			Id:         sc.Id,
+			CommentCtx: &pb.Context_Comment{
+				Id:        sc.Comment.Id,
+				ThreadCtx: &pb.Context_Thread{
+					Id: sc.Comment.Thread.Id,
+					SectionCtx: &pb.Context_section{
+						Name: sc.Comment.Thread.SectionName,
+					},
+				},
+			},
+		}
+		fudActivity.Subcomments = append(fudActivity.Subcomments, pbSubcomment)
+	}
+	return map[string]*pb.FullUserData_Activity{
+		userId: fudActivity
+	}
 }
 
 type Activity struct {
