11b11eb2ed902a4f44f3cd1cc0eb8beae77e25ac
Luis G. Villegas <luisguve@gmail.com>
2020-07-16 15:20:23 -0430

    Updated New to receive bind address and port.

--- internal/app/cherosite/cheropatilla.go
+++ internal/app/cherosite/cheropatilla.go
@@ -11,11 +11,11 @@ func (app *App) Run() error {
 	return app.srv.ListenAndServe()
 }
 
-func New(h http.Handler) *App {
+func New(h http.Handler, addr, port string) *App {
 	return &App{
 		srv: &http.Server{
 			Handler:      h,
-			Addr:         "127.0.0.1:8000",
+			Addr:         addr + port,
 			WriteTimeout: 15 * time.Second,
 			ReadTimeout:  15 * time.Second,
 			IdleTimeout:  time.Second * 60,
