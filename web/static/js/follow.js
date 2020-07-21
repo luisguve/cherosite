6b4e67c8d9271decae77b295b5394e5f97b55fc4
Luis G. Villegas <luisguve@gmail.com>
2020-07-20 20:56:28 -0430

    Introduced script to follow/unfollow users.

--- web/static/js/follow.js
+++ web/static/js/follow.js
@@ -1,6 +1,34 @@
-window.addEventListener("load", function() {
+/*window.addEventListener("load", function() {
 	followDivs = document.getElementsByClassName("follow");
 	for (var i = followDivs.length - 1; i >= 0; i--) {
 		followDivs[i]
 	}
-});
+});*/
+// Script to execute from the console.
+/*
+var req = new XMLHttpRequest();
+req.open("POST", "/follow?username=arodseth");
+req.onreadystatechange = function() {
+	if (this.readyState == 4) {
+		if (this.status == 200) {
+			console.log("success");
+		} else {
+			console.log(this.responseText);
+		}
+	}
+};
+req.send();
+
+var req = new XMLHttpRequest();
+req.open("POST", "/unfollow?username=arodseth");
+req.onreadystatechange = function() {
+	if (this.readyState == 4) {
+		if (this.status == 200) {
+			console.log("success");
+		} else {
+			console.log(this.responseText);
+		}
+	}
+};
+req.send();
+*/
