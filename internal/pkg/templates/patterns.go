4d8b1fd8262b4489d22b29a5d839a0d1476c615c
Luis G. Villegas <luisguve@gmail.com>
2020-07-20 20:45:45 -0430

    Modified FeedPattern.

--- internal/pkg/templates/patterns.go
+++ internal/pkg/templates/patterns.go
@@ -6,29 +6,29 @@ import (
 
 var FeedPattern = []pbMetadata.ContentStatus{
 	pbMetadata.ContentStatus_NEW, // 1
-	pbMetadata.ContentStatus_NEW, // 2
+	pbMetadata.ContentStatus_REL, // 2
 	pbMetadata.ContentStatus_NEW, // 3
 	pbMetadata.ContentStatus_TOP, // 4
-	pbMetadata.ContentStatus_NEW, // 5
-	pbMetadata.ContentStatus_NEW, // 6
+	pbMetadata.ContentStatus_REL, // 5
+	pbMetadata.ContentStatus_REL, // 6
 	pbMetadata.ContentStatus_NEW, // 7
-	pbMetadata.ContentStatus_NEW, // 8
+	pbMetadata.ContentStatus_REL, // 8
 	pbMetadata.ContentStatus_REL, // 9
-	pbMetadata.ContentStatus_NEW, // 10
+	pbMetadata.ContentStatus_REL, // 10
 	pbMetadata.ContentStatus_REL, // 11
-	pbMetadata.ContentStatus_NEW, // 12
+	pbMetadata.ContentStatus_REL, // 12
 	pbMetadata.ContentStatus_NEW, // 13
 	pbMetadata.ContentStatus_REL, // 14
 	pbMetadata.ContentStatus_REL, // 15
 	pbMetadata.ContentStatus_REL, // 16
 	pbMetadata.ContentStatus_NEW, // 17
-	pbMetadata.ContentStatus_NEW, // 18
-	pbMetadata.ContentStatus_NEW, // 19
+	pbMetadata.ContentStatus_REL, // 18
+	pbMetadata.ContentStatus_REL, // 19
 	pbMetadata.ContentStatus_NEW, // 20
 	pbMetadata.ContentStatus_REL, // 21
 	pbMetadata.ContentStatus_REL, // 22
 	pbMetadata.ContentStatus_REL, // 23
-	pbMetadata.ContentStatus_NEW, // 24
+	pbMetadata.ContentStatus_REL, // 24
 }
 
 var CommentPattern = []pbMetadata.ContentStatus{
