0147c98391b76c0387d734bda1104166501388d1
Luis G. Villegas <luisguve@gmail.com>
2020-07-27 13:35:20 -0430

    Modularized setup by introducing function siteConfig.

--- cmd/cherosite/cheropatilla.go
+++ cmd/cherosite/cheropatilla.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/gorilla/sessions"
@@ -13,39 +15,37 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
-	// Get config variables.
-	config, err := godotenv.Read("C:/cherosite_files/config.env")
+func siteConfig(file string, vars ...string) (map[string]string, error) {
+	config, err := godotenv.Read(file)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	environment, ok := config["ENV"]
-	if !ok {
-		log.Fatal("Missing ENV in config.env")
+	var result = make(map[string]string)
+	if len(vars) > 0 {
+		for _, key := range vars {
+			val, ok := config[key]
+			if !ok {
+				errMsg := fmt.Sprintf("Missing %s in %s.", key, file)
+				return nil, errors.New(errMsg)
+			}
+			result[key] = val
+		}
+	} else {
+		result = config
 	}
-	// Get address and port
-	addr, ok := config["BIND_ADDR"]
-	if !ok {
-		log.Fatal("Missing BIND_ADDR in config.env")
-	}
-	port, ok := config["PORT"]
-	if !ok {
-		log.Fatal("Misssing PORT in config.env")
-	}
-	// Get new template engine
-	tpl := templates.Setup(environment, port)
+	return result, nil
+}
 
+func main() {
 	// Get grpc config variables.
-	grpcConfig, err := godotenv.Read("C:/cheroshared_files/grpc_config.env")
+	grpcConfig, err := siteConfig("C:/cheroshared_files/grpc_config.env",
+		"BIND_ADDR")
 	if err != nil {
 		log.Fatal(err)
 	}
-	address, ok := grpcConfig["BIND_ADDR"]
-	if !ok {
-		log.Fatal("Missing BIND_ADDR in grpc_config.env")
-	}
+
 	// Establish connection with gRPC server
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	conn, err := grpc.Dial(grpcConfig["BIND_ADDR"], grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -55,34 +55,42 @@ func main() {
 	ccc := pbApi.NewCrudCheropatillaClient(conn)
 
 	// Get session key.
-	env, err := godotenv.Read("C:/cherosite_files/cookie_hash.env")
+	sess, err := siteConfig("C:/cherosite_files/cookie_hash.env", "SESSION_KEY",
+		"SESS_DIR")
 	if err != nil {
 		log.Fatal(err)
 	}
-	key, ok := env["SESSION_KEY"]
-	if !ok {
-		log.Fatal("Missing SESSION_KEY in cookie_hash.env")
-	}
 
 	// Create session store.
-	store := sessions.NewFilesystemStore("tmp/sess", []byte(key))
+	store := sessions.NewFilesystemStore(sess["SESS_DIR"], []byte(sess["SESSION_KEY"]))
 	// Set no limit on length of sessions.
 	store.MaxLength(0)
+
 	// Create and start hub
 	hub := livedata.NewHub()
 	go hub.Run(ccc)
 
 	// Get section names mapped to their ids.
-	sections, err := godotenv.Read("C:/cheroshared_files/sections.env")
+	sections, err := siteConfig("C:/cheroshared_files/sections.env")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Setup router and routes
+	// Get config variables.
+	config, err := siteConfig("C:/cherosite_files/config.env", "ENV", "BIND_ADDR",
+		"PORT")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup a new templates engine.
+	tpl := templates.Setup(config["ENV"], config["PORT"])
+
+	// Setup router and routes.
 	router := router.New(tpl, ccc, store, hub, sections)
 	router.SetupRoutes()
 
-	// Start app
-	a := app.New(router, addr, port)
+	// Start app.
+	a := app.New(router, config["BIND_ADDR"], config["PORT"])
 	log.Fatal(a.Run())
 }
