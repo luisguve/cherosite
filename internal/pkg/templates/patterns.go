a3b033080bac5cb820987bf747b4e9777f57c2df
Luis G. Villegas <luisguve@gmail.com>
2020-08-28 09:51:06 -0430

    Updated patterns.

--- internal/pkg/templates/patterns.go
+++ internal/pkg/templates/patterns.go
@@ -5,55 +5,37 @@ import (
 )
 
 var FeedPattern = []pbMetadata.ContentStatus{
-	pbMetadata.ContentStatus_NEW, // 1
-	pbMetadata.ContentStatus_REL, // 2
-	pbMetadata.ContentStatus_NEW, // 3
-	pbMetadata.ContentStatus_TOP, // 4
+	pbMetadata.ContentStatus_TOP, // 1
+	pbMetadata.ContentStatus_NEW, // 2
+	pbMetadata.ContentStatus_REL, // 3
+	pbMetadata.ContentStatus_NEW, // 4
 	pbMetadata.ContentStatus_REL, // 5
 	pbMetadata.ContentStatus_REL, // 6
 	pbMetadata.ContentStatus_NEW, // 7
-	pbMetadata.ContentStatus_REL, // 8
+	pbMetadata.ContentStatus_NEW, // 8
 	pbMetadata.ContentStatus_REL, // 9
 	pbMetadata.ContentStatus_REL, // 10
 	pbMetadata.ContentStatus_REL, // 11
-	pbMetadata.ContentStatus_REL, // 12
-	pbMetadata.ContentStatus_NEW, // 13
-	pbMetadata.ContentStatus_REL, // 14
-	pbMetadata.ContentStatus_REL, // 15
-	pbMetadata.ContentStatus_REL, // 16
-	pbMetadata.ContentStatus_NEW, // 17
-	pbMetadata.ContentStatus_REL, // 18
-	pbMetadata.ContentStatus_REL, // 19
-	pbMetadata.ContentStatus_NEW, // 20
-	pbMetadata.ContentStatus_REL, // 21
-	pbMetadata.ContentStatus_REL, // 22
-	pbMetadata.ContentStatus_REL, // 23
-	pbMetadata.ContentStatus_REL, // 24
 }
 
 var CommentPattern = []pbMetadata.ContentStatus{
-	pbMetadata.ContentStatus_REL, // 1
-	pbMetadata.ContentStatus_TOP, // 2
-	pbMetadata.ContentStatus_NEW, // 3
+	pbMetadata.ContentStatus_TOP, // 1
+	pbMetadata.ContentStatus_REL, // 2
+	pbMetadata.ContentStatus_REL, // 3
 	pbMetadata.ContentStatus_REL, // 4
 	pbMetadata.ContentStatus_NEW, // 5
 	pbMetadata.ContentStatus_NEW, // 6
 	pbMetadata.ContentStatus_NEW, // 7
 	pbMetadata.ContentStatus_REL, // 8
-	pbMetadata.ContentStatus_NEW, // 9
-	pbMetadata.ContentStatus_NEW, // 10
-	pbMetadata.ContentStatus_REL, // 11
-	pbMetadata.ContentStatus_REL, // 12
-	pbMetadata.ContentStatus_NEW, // 13
-	pbMetadata.ContentStatus_NEW, // 14
 }
 
 var CompactPattern = []pbMetadata.ContentStatus{
 	pbMetadata.ContentStatus_NEW, // 1
-	pbMetadata.ContentStatus_NEW, // 2
-	pbMetadata.ContentStatus_REL, // 3
-	pbMetadata.ContentStatus_TOP, // 4
+	pbMetadata.ContentStatus_REL, // 2
+	pbMetadata.ContentStatus_TOP, // 3
+	pbMetadata.ContentStatus_REL, // 4
 	pbMetadata.ContentStatus_REL, // 5
 	pbMetadata.ContentStatus_NEW, // 6
 	pbMetadata.ContentStatus_NEW, // 7
+	pbMetadata.ContentStatus_NEW, // 8
 }
