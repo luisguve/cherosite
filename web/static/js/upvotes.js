1437ca3a08a1be1287816b6149e669618101e946
Luis G. Villegas <luisguve@gmail.com>
2020-08-28 09:51:08 -0430

    Set event listeners if topArticle !== null.

--- web/static/js/upvotes.js
+++ web/static/js/upvotes.js
@@ -41,45 +41,44 @@ for (var i = articles.length - 1; i >= 0; i--) {
 	};
 }
 var topArticle = document.querySelector("article[class*='TOP']");
-if (topArticle == null) {
-	return;
+if (topArticle !== null) {
+	var upvoteLink = topArticle.dataset["upvoteLink"];
+	var undoUpvoteLink = topArticle.dataset["undoUpvoteLink"];
+	let btn = topArticle.querySelector(".upvotes > button");
+	btn.onclick = function() {
+		let upvoted = btn.dataset["upvoted"];
+		let req = new XMLHttpRequest();
+		if (upvoted == "true") {
+			// Post request to undo upvote.
+			req.open("POST", undoUpvoteLink);
+			req.onreadystatechange = function() {
+				if (this.readyState == 4) {
+					if (this.status == 200) {
+						btn.dataset["upvoted"] = false;
+						let upvotes = parseInt(btn.innerHTML);
+						upvotes--;
+						btn.innerHTML = upvotes + " Upvotes";
+					} else {
+						console.log(this.responseText);
+					}
+				}
+			};
+		} else {
+			// Post request to upvote.
+			req.open("POST", upvoteLink);
+			req.onreadystatechange = function() {
+				if (this.readyState == 4) {
+					if (this.status == 200) {
+						btn.dataset["upvoted"] = true;
+						let upvotes = parseInt(btn.innerHTML);
+						upvotes++;
+						btn.innerHTML = upvotes + " Upvotes";
+					} else {
+						console.log(this.responseText);
+					}
+				}
+			};
+		}
+		req.send();
+	};
 }
-var upvoteLink = topArticle.dataset["upvoteLink"];
-var undoUpvoteLink = topArticle.dataset["undoUpvoteLink"];
-let btn = topArticle.querySelector(".upvotes > button");
-btn.onclick = function() {
-	let upvoted = btn.dataset["upvoted"];
-	let req = new XMLHttpRequest();
-	if (upvoted == "true") {
-		// Post request to undo upvote.
-		req.open("POST", undoUpvoteLink);
-		req.onreadystatechange = function() {
-			if (this.readyState == 4) {
-				if (this.status == 200) {
-					btn.dataset["upvoted"] = false;
-					let upvotes = parseInt(btn.innerHTML);
-					upvotes--;
-					btn.innerHTML = upvotes + " Upvotes";
-				} else {
-					console.log(this.responseText);
-				}
-			}
-		};
-	} else {
-		// Post request to upvote.
-		req.open("POST", upvoteLink);
-		req.onreadystatechange = function() {
-			if (this.readyState == 4) {
-				if (this.status == 200) {
-					btn.dataset["upvoted"] = true;
-					let upvotes = parseInt(btn.innerHTML);
-					upvotes++;
-					btn.innerHTML = upvotes + " Upvotes";
-				} else {
-					console.log(this.responseText);
-				}
-			}
-		};
-	}
-	req.send();
-};
